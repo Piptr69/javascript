d8e90b53443477e24ea5f5b449643c8bd80093f7
Piptr69 <p.paluchh02@gmail.com>
2024-01-10 22:27:56 +0100

    now it works

--- learning-strings/javascript.js
+++ learning-strings/javascript.js
@@ -1,3 +1,5 @@
-const badString1 = This is a test;
-const badString2 = 'This is a test;
-const badString3 = This is a test';
+const string = "The revolution will not be televised.";
+console.log(string);
+
+const badString = string;
+console.log(badString);
