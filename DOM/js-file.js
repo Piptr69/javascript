30f0e811cda3e707675b6ba042d94d5bedc09f66
Piptr69 <p.paluchh02@gmail.com>
2024-01-23 18:56:08 +0100

    add paragraph text

--- DOM/js-file.js
+++ DOM/js-file.js
@@ -5,3 +5,9 @@ content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
 
 container.appendChild(content);
+
+const p = document.createElement('p');
+p.classList.add('p');
+p.textContent = "Hey I'm red!";
+
+container.appendChild(p);
